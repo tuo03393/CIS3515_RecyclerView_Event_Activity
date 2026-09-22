785aabe83b5715a1edd1b593d40a7c43ae604dd4
tuo03393 <tuo03393@temple.edu>
2026-09-22 12:21:32 -0400

    Commit A

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -16,14 +16,23 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val displayTextView = findViewById<TextView>(R.id.displayTextView)
         recyclerView.layoutManager = GridLayoutManager(this, 4)
-
+/*
         // TODO (Step 2: Define lambda to modify displayTextView size)
-        val callback : (Int) -> Unit = {}
+        val callback = {
+            //Generic lambdas use "it".
+            textSizeR : Int ->
+            displayTextView.textSize = textSizeR.toFloat()
+        }
 
 
         // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
-
+        recyclerView.adapter = NumberDisplayAdapter(numbers, callback)
+*/
+       // /* You can also do, instead of step 2 & 3, do:
+        recyclerView.adapter = NumberDisplayAdapter(numbers) {
+        displayTextView.textSize = it.toFloat()
+        }
+       //  */
 
     }
 
--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -7,14 +7,20 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // TODO(Step 1: Implement click behavior)
-
 // TODO (Step 1a: Accept lambda in constructor)
-class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+//Define an argument to accept a certain type. For a lambda, it will be the signature of the lambda. What does it take & what does it return.
+//In this case, it is listed in step 1b.
+//Added private val callback : (Int) -> Unit.
+class NumberDisplayAdapter (private val numbers: Array<Int>, private val callback: (Int) -> Unit) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
     // TODO (Step 1b: Invoke lambda via onClickListener)
     inner class NumberViewHolder (layout: View) : RecyclerView.ViewHolder (layout) {
         // enumerate views inside layout
-        val textView = layout.findViewById<TextView>(R.id.textView)
+        val textView = layout.findViewById<TextView>(R.id.textView).apply { //Added .apply & the setOnClickListener function.
+            setOnClickListener{
+                callback(numbers[bindingAdapterPosition])
+            }
+        }
 
     }
 
@@ -23,6 +29,8 @@ class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adap
         val layout = LayoutInflater.from(parent.context).inflate(R.layout.textview_layout, parent, false)
         return NumberViewHolder(layout)
     }
+    //Inflate is the term for turning any resources into an object in memory.
+    //Inflate wants three arguments: layout, root, and attach to root, which is usually false.
 
     override fun getItemCount(): Int {
         return numbers.size
